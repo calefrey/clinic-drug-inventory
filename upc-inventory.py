73bcb6dc52920ffa5ccb7c9b4d3ff2bbc7a28b47
Caleb Frey <caleb.frey@rutgers.edu>
2020-10-08 14:42:07 -0400

    include message if not found

--- upc-inventory.py
+++ upc-inventory.py
@@ -15,25 +15,23 @@ while True:
         name = results["brand_name"]
         strength = results["active_ingredients"][0]["strength"]
         qty = results["packaging"][0]["description"]
-        
 
         print(f"{name}, {strength}")
         print(f"Qty: {qty}")
         print("Is this info correct? (y/n)")
         correct = input()
-        if correct == 'y':
-            cost = input("Enter cost per bottle: ")
+        if correct == "y":
             stock = input("Enter number of bottles: ")
         else:
             print("Ok, add it manually")
             name = input("Name: ")
             strength = input("Strength: ")
-            cost = input("Enter cost per bottle: ")
             stock = input("Enter number of bottles: ")
 
-
-        identifier = {'_id': upc}
-        product = {'name': name, 'strength': strength, 'qty': qty, 'cost':cost, 'stock': stock}
+        identifier = {"_id": upc}
+        product = {"name": name, "strength": strength, "qty": qty, "stock": stock}
         drugs.replace_one(identifier, product, True)
         print("Added to database")
         print("\n\n")
+    else:
+        print("Not found in database")
