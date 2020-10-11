3cba48a858fefceb91d94958e8d18e1c18bc1c3e
Caleb Frey <caleb.frey@rutgers.edu>
2020-10-11 01:14:09 -0400

    error handling and skipping

--- add-prices.py
+++ add-prices.py
@@ -1,10 +1,12 @@
 import creds
 from pymongo import MongoClient
 from bson.decimal128 import Decimal128 as decimal
+
+print("To skip entering a price, set the it to 0 and we'll come back to it next time")
 client = MongoClient(creds.mongostring)
 db = client.inventory
 drugs = db.drugs
-data = drugs.find({"price": {"$exists": False}}).sort("name")
+data = drugs.find({"$or": [{"price": {"$exists": False}}, {"price": 0}]}).sort("name")
 for doc in data:
     name = doc["name"]
     strength = doc["strength"]
@@ -12,5 +14,10 @@ for doc in data:
     print(f"Price for {name}, {strength}")
     print(qty)
     price = input("$")
+    try: #make sure it's a number
+        float(price)
+    except ValueError:
+        print("Careful, that's not a number!")
+        price = input("$")
     drugs.update_one({"_id": doc["_id"]}, {"$set": {"price": decimal(price)}})
 print("All records have prices")
